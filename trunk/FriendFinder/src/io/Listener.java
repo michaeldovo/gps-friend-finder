73bff2d2c6ad13bca1b2878d61b9349bab2e34d1
christian.winkler.007 <christian.winkler.007@1881b811-d24d-0410-81dc-a53cb171cb69>
2008-05-27 09:52:04 +0000

    Immer noch ziemliches leeres Grundgerüst mit aufgeteilten Paketen und den vorgefertigten Connections außem Moodle

---
+++ trunk/FriendFinder/src/io/Listener.java
@@ -0,0 +1,13 @@
+package io;
+
+/**
+ *
+ * @author Alireza Sahami
+ * Pervasive Computing Group, Univeristy of Duisburg-Essen
+ */
+public interface Listener {
+    
+    /** Creates a new instance of Listener */
+    public void printMsg (String s);
+    
+}
---
+++ trunk/FriendFinder/src/main/Property.java
@@ -0,0 +1,15 @@
+package main;
+
+/**
+ * Gemeinsame Property-Datei 
+ */
+
+public class Property {
+// ************
+// BLUETOOTH
+// ************
+    // bluetoth device name
+    public static final String btName="BT-GPS-380D4E";
+    
+    
+}
